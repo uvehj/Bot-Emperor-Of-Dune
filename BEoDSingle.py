fec90228980adfd4c90b3184b0562fae64441364
uvehj <49170460+uvehj@users.noreply.github.com>
2019-04-01 23:27:49 +0200

    Added comments, removed minor error

--- BEoDSingle.py
+++ BEoDSingle.py
@@ -11,13 +11,15 @@ with open("./geod.txt") as f:
 
 # Build the model.
 text_model = markovify.NewlineText(text)
-
+#Creates the first sentence, max characters to the twitter max
 text_body = text_model.make_short_sentence(240, max_overlap_ratio = 0.5)
 if text_body != "None":
+	#Removes blanks preceding characters (the only 3 that appear in the text)
 	text_body = text_body.replace(" ,", ",")
 	text_body = text_body.replace(" ?", "?")
 	text_body = text_body.replace(" !", "!")
 	i = 0
+	#If the sentence is too short adds more sentences to get to at least half the max characters
 	while len(text_body) < 110 and i < 50:
 		i = i + 1
 		new_text_body = text_model.make_short_sentence(110, max_overlap_ratio = 0.5)
@@ -27,27 +29,29 @@ if text_body != "None":
 			new_text_body = new_text_body.replace(" !", "!")
 			text_body = text_body + '. ' + new_text_body
 
+	#Final period and removal of unnecessary periods
 	text_body = text_body + '.'
 	text_body = text_body.replace("?.", "?")
 	text_body = text_body.replace("!.", ".")
 	text_body = text_body.replace(",.", ",")
+	#If there is space it adds a generic Dune hashtag
 	if len(text_body) < 250 and i < 50:
 		text_body = text_body + ' ' + "#Dune"
 
-
+	#Here be the twitter keys, don't make them public (I know this is not the safest way to go about it)
 	apiKey = ""
-	apiSecret = "y"
+	apiSecret = ""
 	accessToken = ""
 	accessTokenSecret = ''
 
 	api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
 
+	#Send tweet
 	api.update_status(status=text_body)
 	print(datetime.datetime.now())
 	print("Tweeted: " + text_body)
 
 else:
+	#In case the sentence generation goes wrong
 	print(datetime.datetime.now())
 	print("Error while creating the sentence")
-
-log_file.close() 
